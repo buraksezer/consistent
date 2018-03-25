88bdb9b8af8d77a61a1a407896c85c5371a20207
Burak Sezer <burak.sezer.developer@gmail.com>
2018-03-25 23:35:58 +0300

    add hasher again

--- consistent.go
+++ consistent.go
@@ -17,6 +17,14 @@ var (
 	ErrMemberNotFound        = errors.New("member could not be found in circle")
 )
 
+// Hasher is responsible for generating unsigned, 64 bit hash of provided byte slice.
+// Hasher should minimize collisions (generating same hash for different byte slice)
+// and while performance is also important fast functions are preferable (i.e.
+// you can use FarmHash family).
+type Hasher interface {
+	Sum64([]byte) uint64
+}
+
 type Member interface {
 	Name() string
 }
