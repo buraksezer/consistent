bb03a1d5fe208c6c7c5e7c2906ad77ad97ea64cd
Burak Sezer <burak.sezer.developer@gmail.com>
2018-03-26 17:08:40 +0300

    add a new example: locate_keys

---
+++ _examples/locate_keys.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/buraksezer/consistent"
+	"github.com/cespare/xxhash"
+)
+
+type Member string
+
+func (m Member) Name() string {
+	return string(m)
+}
+
+type hasher struct{}
+
+func (h hasher) Sum64(data []byte) uint64 {
+	return xxhash.Sum64(data)
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func main() {
+	members := []consistent.Member{}
+	for i := 0; i < 8; i++ {
+		member := Member(fmt.Sprintf("node%d.olricmq", i))
+		members = append(members, member)
+	}
+	cfg := &consistent.Config{
+		PartitionCount:    271,
+		ReplicationFactor: 40,
+		LoadFactor:        1.2,
+		Hasher:            hasher{},
+	}
+	c := consistent.New(members, cfg)
+
+	keyCount := 1000000
+	load := (c.AverageLoad() * float64(keyCount)) / float64(cfg.PartitionCount)
+	fmt.Println("Maximum key count for a member should be around this: ", math.Ceil(load))
+	distribution := make(map[string]int)
+	key := make([]byte, 4)
+	for i := 0; i < keyCount; i++ {
+		rand.Read(key)
+		member := c.LocateKey(key)
+		distribution[member.Name()]++
+	}
+	for member, count := range distribution {
+		fmt.Printf("member: %s, key count: %d\n", member, count)
+	}
+}
